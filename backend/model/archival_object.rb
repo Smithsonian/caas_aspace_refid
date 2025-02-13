f7b1a1eae51f21c4093dfa61b3a95c324a4d57ff
Lora Woodford <lorawoodford@gmail.com>
2025-02-13 13:51:04 -0500

    [BUG] Import via spreadsheet fails after first AO #33

--- backend/model/archival_object.rb
+++ backend/model/archival_object.rb
@@ -2,23 +2,18 @@ require 'net/http'
 require 'date'
 
 def generate_ref_id(resource)
-  next_refid = get_next_refid(resource)
-
   begin
     url = URI.parse(AppConfig[:backend_url] + "/plugins/caas_next_refid?resource_id=#{resource.id}")
     request = Net::HTTP::Post.new(url.to_s)
-    Net::HTTP.start(url.host, url.port) do |http|
+    response = Net::HTTP.start(url.host, url.port) do |http|
       http.request(request)
     end
-    next_refid
+    JSON(response.body)['next_refid'] - 1
   rescue
     return DateTime.now.strftime('%Q')
   end
 end
 
-def get_next_refid(resource)
-  resource['caas_next_refid'] ? resource['caas_next_refid']['next_refid'] : 1
-end
 
 ArchivalObject.auto_generate(property: :ref_id,
                              generator: proc do |json|
--- backend/spec/model_archival_object_spec.rb
+++ backend/spec/model_archival_object_spec.rb
@@ -6,110 +6,57 @@ describe 'ArchivalObject model' do
 
   context 'when the next_refid endpoint responds' do
     let(:response) { instance_double(Net::HTTPResponse) }
+    let(:caas_next_refid) do
+      JSONModel(:caas_next_refid).from_hash({ resource_id: resource.id,
+                                              next_refid: 41 })
+    end
 
     before do
       allow(Net::HTTP).to receive(:start).and_return(response)
+      allow(response).to receive(:body).and_return(caas_next_refid.to_json)
     end
 
-    context 'when a CaasAspaceRefid record exists' do
-      let(:caas_next_refid) do
-        JSONModel(:caas_next_refid).from_hash({ resource_id: resource.id,
-                                                next_refid: 40 })
-      end
-
-      before do
-        CaasAspaceRefid.create_from_json(caas_next_refid)
-      end
-
-      describe '#generate_ref_id' do
-        it 'returns the existing refid' do
-          expect(generate_ref_id(resource_json)).to eq(40)
-        end
-      end
-
-      describe '#auto_generate' do
-        context 'when the archival object is new' do
-          let(:archival_object) do
-            create_archival_object({ ref_id: nil,
-                                     resource: { ref: "/repositories/2/resources/#{resource.id}" } })
-          end
-
-          it 'calls the caas_next_refid endpoint' do
-            archival_object
-
-            expect(Net::HTTP).to have_received(:start)
-          end
-
-          it 'auto generates the ref_id' do
-            expect(archival_object.ref_id).to eq('my.eadid_ref40')
-          end
-        end
-
-        context 'when an existing archival object is updated with caas_regenerate_ref_id set to true' do
-          let(:archival_object) do
-            create_archival_object({ ref_id: '123',
-                                     caas_regenerate_ref_id: true,
-                                     resource: { ref: "/repositories/2/resources/#{resource.id}" } })
-          end
-
-          it 'calls the caas_next_refid endpoint' do
-            archival_object
-
-            expect(Net::HTTP).to have_received(:start)
-          end
-
-          it 'auto generates ref_id' do
-            archival_object.save
-
-            expect(archival_object.ref_id).to eq('my.eadid_ref40')
-          end
-        end
+    describe '#generate_ref_id' do
+      it 'returns a value one less than the next_refid' do
+        expect(generate_ref_id(resource_json)).to eq(40)
       end
     end
 
-    context 'when a CaasAspaceRefid does not exist' do
-      describe '#generate_ref_id' do
-        it 'returns 1' do
-          expect(generate_ref_id(resource_json)).to eq(1)
+    describe '#auto_generate' do
+      context 'when the archival object is new' do
+        let(:archival_object) do
+          create_archival_object({ ref_id: nil,
+                                    resource: { ref: "/repositories/2/resources/#{resource.id}" } })
+        end
+
+        it 'calls the caas_next_refid endpoint' do
+          archival_object
+
+          expect(Net::HTTP).to have_received(:start)
+        end
+
+        it 'auto generates the ref_id' do
+          expect(archival_object.ref_id).to eq('my.eadid_ref40')
         end
       end
 
-      describe '#auto_generate' do
-        context 'when the archival object is new' do
-          let(:archival_object) do
-            create_archival_object({ ref_id: nil,
-                                     resource: { ref: "/repositories/2/resources/#{resource.id}" } })
-          end
-
-          it 'calls the caas_next_refid endpoint' do
-            archival_object
-
-            expect(Net::HTTP).to have_received(:start)
-          end
-
-          it 'auto generates the ref_id' do
-            expect(archival_object.ref_id).to eq('my.eadid_ref1')
-          end
+      context 'when an existing archival object is updated with caas_regenerate_ref_id set to true' do
+        let(:archival_object) do
+          create_archival_object({ ref_id: '123',
+                                    caas_regenerate_ref_id: true,
+                                    resource: { ref: "/repositories/2/resources/#{resource.id}" } })
         end
 
-        context 'when an existing archival object is updated with caas_regenerate_ref_id set to true' do
-          let(:archival_object) do
-            create_archival_object({ ref_id: '123',
-                                     caas_regenerate_ref_id: true,
-                                     resource: { ref: "/repositories/2/resources/#{resource.id}" } })
-          end
+        it 'calls the caas_next_refid endpoint' do
+          archival_object
 
-          it 'calls the caas_next_refid endpoint' do
-            archival_object
+          expect(Net::HTTP).to have_received(:start)
+        end
 
-            expect(Net::HTTP).to have_received(:start)
-          end
+        it 'auto generates ref_id' do
+          archival_object.save
 
-          it 'auto generates ref_id' do
-            archival_object.save
-
-            expect(archival_object.ref_id).to eq('my.eadid_ref1')
-          end
+          expect(archival_object.ref_id).to eq('my.eadid_ref40')
         end
       end
     end
@@ -146,7 +93,7 @@ describe 'ArchivalObject model' do
         end
       end
 
-      context 'when archival object updated with caas_regenerate_ref_id set to true' do
+      context 'when an existing archival object is updated with caas_regenerate_ref_id set to true' do
         let(:archival_object) do
           create_archival_object({ caas_regenerate_ref_id: true,
                                    resource: { ref: "/repositories/2/resources/#{resource.id}" } })
